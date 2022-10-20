b95bff89afd861c2ac7a10923cf014f2972cb30e
Ivan Craig <ic188002@falmouth.ac.uk>
2022-10-20 16:08:30 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Signup from './user/Signup'
+import Signin from './user/Signin'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import Axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Alert } from "react-bootstrap"
+//ROUTING
 
-function App() {
+
+export default function App() {
+
+  
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState({});
+  const [message, setMessage] = useState(null);
+  //this will be exicuted 
+  useEffect(() => {
+    let token = localStorage.getItem("token")
+    if (token != null) {
+      let user = jwt_decode(token);
+      if (user) {
+        setIsAuth(true)
+        setUser(user);
+      }
+      else if (!user) {
+        localStorage.removeItem("token");
+        setIsAuth(false)
+
+      }
+    }
+  }, [])
+
+
+
+  const registerHandler = (user) => {
+    Axios.post("auth/signup", user)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }
+
+
+  const loginHandler = (cred) => {
+    Axios.post("auth/signin", cred)
+      .then(response => {
+        console.log(response.data.token);
+        if (response.data.token != null) {
+          localStorage.setItem("token", response.data.token)
+          let user = jwt_decode(response.data.token)
+          setIsAuth(true);
+          setUser(user)
+          setMessage("User Logged Out Successfully")
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+
+  const onLogoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token")
+    setIsAuth(false)
+    setUser(null)
+  }
+
+const errMessage = message ? (
+  <Alert variant='success'>{message}</Alert>
+) : null
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. These are some changes made on Rob's dev
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        {errMessage}
+        <nav>
+          {isAuth ? (
+            <div>
+              {user ? "welcome" + user.user.id : null}&nbsp;
+              <Link to='/'>Home</Link>&nbsp;
+              <Link to='/logout' onClick={onLogoutHandler}>Logout</Link>&nbsp;
+            </div>
+          ) : (
+            <div>
+              <Link to='/signup'>Signup</Link>&nbsp;
+              <Link to='/signin'>Signin</Link>&nbsp;
+              <Link to='/'>Home</Link>&nbsp;
 
-export default App;
+            </div>
+          )}
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" element={<Signin login={loginHandler} />}></Route>
+            <Route path="/signup" element={<Signup register={registerHandler} />}></Route>
+            <Route path="/signin" element={<Signin login={loginHandler} />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
+}
---
+++ src/user/Signup.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import { Container, Form, Button} from "react-bootstrap"
+
+
+
+
+export default function SignUp(props) {
+    const [newUser, setNewUser] = useState({})
+    const changeHandler = (e) => {
+        const user = { ...newUser };
+        user[e.target.name] = e.target.value;
+        console.log(user);
+        setNewUser(user);
+    }
+
+    const registerHandler = () => {
+        props.register(newUser)
+        }
+  return (
+    <div>
+    <Container>
+        <Form.Group>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control name='firstName' onChange={changeHandler}></Form.Control>
+        </Form.Group>
+
+
+        <Form.Group>
+            <Form.Label>last Name</Form.Label>
+            <Form.Control name='lastName' onChange={changeHandler}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control name='emailAddress' onChange={changeHandler}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control name='password' type="password" onChange={changeHandler}></Form.Control>
+        </Form.Group>
+        <Button variant="primary" onClick={registerHandler}>Register</Button>
+
+    </Container></div>
+  )
+}
