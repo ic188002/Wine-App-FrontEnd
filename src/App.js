51a64bc890982e6046c1eab0decccc05be5b3895
Ivan Craig <ic188002@falmouth.ac.uk>
2022-10-21 09:19:49 +0100

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
+
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import Axios from 'axios'
 import jwt_decode from 'jwt-decode'
@@ -10,7 +11,7 @@ import { Alert } from "react-bootstrap"
 
 export default function App() {
 
-  
+
   const [isAuth, setIsAuth] = useState(false);
   const [user, setUser] = useState({});
   const [message, setMessage] = useState(null);
@@ -89,6 +90,7 @@ const errMessage = message ? (
               <Link to='/signup'>Signup</Link>&nbsp;
               <Link to='/signin'>Signin</Link>&nbsp;
               <Link to='/'>Home</Link>&nbsp;
+      
 
             </div>
           )}
@@ -98,6 +100,7 @@ const errMessage = message ? (
             <Route path="/" element={<Signin login={loginHandler} />}></Route>
             <Route path="/signup" element={<Signup register={registerHandler} />}></Route>
             <Route path="/signin" element={<Signin login={loginHandler} />}></Route>
+
           </Routes>
         </div>
       </div>
