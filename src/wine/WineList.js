cfd85c4c2678a73ae7df3d8e1e8e21c1a9733026
Ivan Craig <ic188002@falmouth.ac.uk>
2022-10-21 15:50:44 +0100

    11

--- src/wine/WineList.js
+++ src/wine/WineList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
-
+import { Container, Card, Button, CardGroup, Row, Col } from 'react-bootstrap';
 
 export default function WineList() {
   return (
@@ -19,15 +19,89 @@ export default function WineList() {
       <p><Link to="/shirazwinelist">Shiraz Wines</Link></p>
       <p><Link to="/riojawinelist">Rioja Wines</Link></p>
       <p><Link to="/cotesdurhonewinelist">Cotes Du Rhone Wines</Link></p>
-        <p><Link to="/bordeauxwinelist">Bordeaux Wines</Link></p>
-        <p><Link to="/pinotgrigiowinelist">Pinot Grigio Wines</Link></p>
-        <p><Link to="/primitivowinelist">Primitivo Wines</Link></p>
-        <p><Link to="/sauvignonwinelist">Sauvignon Blanc Wines</Link></p>
-        <p><Link to="/cheninblancwinelist"> Chenin Blanc Wines</Link></p>
+      <p><Link to="/bordeauxwinelist">Bordeaux Wines</Link></p>
+      <p><Link to="/pinotgrigiowinelist">Pinot Grigio Wines</Link></p>
+      <p><Link to="/primitivowinelist">Primitivo Wines</Link></p>
+      <p><Link to="/sauvignonwinelist">Sauvignon Blanc Wines</Link></p>
+      <p><Link to="/cheninblancwinelist"> Chenin Blanc Wines</Link></p>
 
 
+
+    <div className='Row'>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Chardonnay Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Riesling Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/red.png"/>
+        <h2 className='info'>Moscato Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Cabernet Sauvignon Wines</h2>
+      </div>
     </div>
 
+    <div className='Row'>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Rose Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/red.png"/>
+        <h2>Malbec Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Chardonnay Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/red.png"/>
+        <h2>Port Wines</h2>
+      </div>
+    </div>
+
+    <div className='Row'>
+      <div className='Box'>
+        <img className='Bottle' src="/images/red.png"/>
+        <h2>Shiraz Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/red.png"/>
+        <h2>Rioja Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Cotes Du Rhone Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/red.png"/>
+        <h2>Bordeaux Wines</h2>
+      </div>
+    </div>
+    <div className='Row'>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Pinot Grigio Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Primitivo Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Sauvignon Blanc Wines</h2>
+      </div>
+      <div className='Box'>
+      <img className='Bottle' src="/images/white.png"/>
+        <h2>Chenin Blanc Wines</h2>
+      </div>
+    </div>
+</div>
 
   )
 }
