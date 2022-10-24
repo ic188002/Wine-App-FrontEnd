fb6e082aa6753630ac9bf6cc5362433aeb09953e
sesemann <robsesemann@gmail.com>
2022-10-24 17:12:24 +0100

    trying to save favourite wine list

--- src/favourites/FavouritesCreateForm.js
+++ src/favourites/FavouritesCreateForm.js
@@ -4,8 +4,8 @@ import React, { useState } from 'react'
 
 
 export default function FavouritesCreateForm(props) {
-
-    const [newWineNight, setNewWineNight] = useState({})
+    
+    const [newWineNight, setNewWineNight] = useState({user: props.user})
 
     const handleChange = (event) => {
         const attributeToChange = event.target.name
@@ -47,7 +47,7 @@ export default function FavouritesCreateForm(props) {
                     </select>
                 </div>
                 <div>
-                    <input name='user' type='text' value={`${props.user}`} onChange={handleChange}></input>
+                    <input name='user' type='hidden' value={`${props.user}`}></input>
                 </div>
 
                 <div>
