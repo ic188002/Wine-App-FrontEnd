9f4f84a66271ed3ece8e3efe9086488bcc261596
Ivan Craig <ic188002@falmouth.ac.uk>
2022-10-26 13:58:00 +0100

    27

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ import FavouritesCreateForm from './favourites/FavouritesCreateForm'
 import FavouriteList from './favourites/FavouriteList'
 import Profile from './user/Profile'
 
-
-
+//////////////////////////////////////////////////////////////////
+import redWineData from  './redWineClassificationData'
 
 
 //ROUTING
 
 
 export default function App() {
+//////////////////////////////////////////////////////////////////
+const redWinecaregories = redWineData.wines
 
 const wineCategories = wineData.wines
 
@@ -114,30 +116,31 @@ const wineCategories = wineData.wines
   }
 
 
-
   useEffect(() => {
     loadWineNight()
 
   }, [])
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
   // This function load the winelist when a wine on the index is clicked 
-  // const loadRedWineIndex = (category) => {
-  //   // it take the  winedata object as a parameter 
-  //   //  it passes the Url through the API to generate get the list of wines 
-  //   axios.get(`https://api.spoonacular.com/food/wine/recommendation?wine=${category.url}&number=6&apiKey=7a64dea7d5bb41f38bb3b24933947711`)
+  const loadRedWineIndex = (category) => {
+    // it take the  winedata object as a parameter 
+    //  it passes the Url through the API to generate get the list of wines 
+    axios.get(`https://api.spoonacular.com/food/wine/recommendation?wine=${category.url}&number=6&apiKey=7a64dea7d5bb41f38bb3b24933947711`)
 
-  //     .then(response => {
-  //       // console.log(response.data)
-  //       // we fetch the data from the API i save it in a variable call winelist 
-  //       setWineList(response.data.recommendedWines)
-  //       // we fetch the data from our classfication data and save it in a variable called wineCategory
-  //       setWineCategory(category)
-  //     })
-  //     .catch(error => {
-  //       console.log('Error Retreving Wines ')
-  //       console.log(error)
-  //     })
-  // }
+      .then(response => {
+        // console.log(response.data)
+        // we fetch the data from the API i save it in a variable call winelist 
+        setWineList(response.data.recommendedWines)
+        // we fetch the data from our classfication data and save it in a variable called wineCategory
+        setRedWineCategory(category)
+      })
+      .catch(error => {
+        console.log('Error Retreving Wines ')
+        console.log(error)
+      })
+  }
 
 
 
---
+++ src/redWineClassificationData.js
@@ -0,0 +1,59 @@
+// as Spooncular did not have an api for classification created an database of these straight from there documents
+// added key value pairs for  type (for index display) & url so we had the right text to feed into the API 
+
+const redWineData = {
+ wines: [
+    {
+        name: "Bordeaux",
+        type: "red",
+        url: "bordeaux"
+    },
+    {
+        name: "Cabernet Sauvignon",
+        type: "red",
+        url: "cabernet_sauvignon"
+    },
+
+
+
+    {
+        name: "Malbec",
+        type: "red",
+        url: "malbec"
+    },
+    
+
+    {
+        name: "Port",
+        type: "red",
+        url:"port"
+    },
+
+
+
+    {
+        name: "Rioja",
+        type: "red",
+        url: "rioja"
+    },
+
+
+
+    {
+        name: "Shiraz",
+        type: "red",
+        url: "shiraz"
+    },
+
+    {   
+        name: "Moscato",
+        type: "red",
+        url: "moscato"
+    }
+
+   
+]
+
+}
+
+export default redWineData;
